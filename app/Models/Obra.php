41343de304ea73cf5f4cbf101c5593088617caa6
Alejandro <alejandro6chaves@gmail.com>
2024-05-14 18:17:54 -0600

    creacion del model y controller obra

---
+++ app/Models/Obra.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Obra extends Model
+{
+    use HasFactory;
+    protected static $tecnicasArte = [
+        'Óleo sobre lienzo',
+        'Acuarela',
+        'Acuarela sobre papel',
+        'Témpera',
+        'Pintura al pastel',
+        'Pintura al fresco',
+        'Pintura digital',
+        'Talla en madera',
+        'Escultura en mármol',
+        'Grabado',
+        'Serigrafía',
+        'Fotografía artística',
+        'Arte digital',
+        'Collage',
+        'Pirograbado',
+        'Escultura en bronce'
+    ];
+    public function imagenes(){
+        return $this->hasMany(Imagen::class, 'idPelicula');
+    }
+
+    public static function getTecnica(){return self::$tecnicasArte;}
+}
